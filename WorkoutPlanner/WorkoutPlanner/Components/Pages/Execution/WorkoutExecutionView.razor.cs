e1b799286910307fea5675470d284aa3d35399d8
Freesbe22 <61194179+Freesbe22@users.noreply.github.com>
2025-05-06 19:42:12 +0200

    tmp

---
+++ WorkoutPlanner/WorkoutPlanner/Components/Pages/Execution/WorkoutExecutionView.razor.cs
@@ -0,0 +1,61 @@
+﻿using Firebase.Auth;
+using Microsoft.AspNetCore.Components;
+using WorkoutPlanner.DataObject;
+using WorkoutPlanner.Tools.Services;
+
+namespace WorkoutPlanner.Components.Pages.Execution
+{
+    partial class WorkoutExecutionView : ComponentBase
+    {
+        #region Variables
+        [Inject]
+        public FirestoreService FirestoreService { get; set; }
+        [Inject]
+        public FirebaseAuthClient AuthClient { get; set; }
+        private List<WorkoutPlan> Programs { get; set; } = new List<WorkoutPlan>();
+        private WorkoutPlan? Program { get; set; }
+        private ProgramPhase? Phase { get; set; }
+        private bool Initialised { get; set; } = false;
+        private bool IsModalVisible { get; set; } = false;
+        public void Toggle ()
+        {
+            IsModalVisible = !IsModalVisible;
+        }
+        #endregion
+
+        #region Loading
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadPrograms();
+            Initialised = true;
+            await InvokeAsync(() => { StateHasChanged(); });
+
+            await base.OnInitializedAsync();
+        }
+
+        protected async Task OnProgramChanged()
+        {
+            if(Program is null)
+            {
+                await LoadPrograms();
+            }
+            else 
+            {
+                await FirestoreService.UpdateObjectReference(FirestoreService.db.Collection(typeof(WorkoutPlan).Name).Document(Program.Id), Program);
+            }
+            await InvokeAsync(() => { StateHasChanged(); });
+        }
+
+        private async Task LoadPrograms()
+        {
+            ///WIP Tmp
+            var firebaseObjectsPublic = await FirestoreService.db.Collection(typeof(WorkoutPlan).Name).WhereEqualTo(nameof(WorkoutPlan.IsPublic), true).GetSnapshotAsync();
+            var firebaseObjectsUser = await FirestoreService.db.Collection(typeof(WorkoutPlan).Name).WhereEqualTo(nameof(WorkoutPlan.UserId), AuthClient.User.Uid).GetSnapshotAsync();
+            firebaseObjectsPublic.ToList().ForEach(firebaseObject => Programs.Add(firebaseObject.ConvertTo<WorkoutPlan>()));
+            firebaseObjectsUser.ToList().ForEach(firebaseObject => Programs.Add(firebaseObject.ConvertTo<WorkoutPlan>()));
+
+            Program = Programs.FirstOrDefault();
+        }
+        #endregion
+    }
+}
